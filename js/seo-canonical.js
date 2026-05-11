8f6acf51f0124433585703b6fd7ea853bb336963
globalconceptsmediallc-oss <globalconceptsmediallc@gmail.com>
2026-05-11 09:43:42 -0400

    Update seo-canonical.js

--- js/seo-canonical.js
+++ js/seo-canonical.js
@@ -1,28 +1,23 @@
 (function () {
-  var origin = window.location.origin;
+  var preferredOrigin = "https://www.a1actionsafeandlock.com";
   var path = window.location.pathname;
 
-  // Normalize homepage
   path = path.replace(/\/index\.html$/i, "/");
 
-  // Remove .html from all pages except root
   if (path !== "/" && path.endsWith(".html")) {
     path = path.replace(/\.html$/i, "");
   }
 
-  // Remove trailing slash (except homepage)
   if (path.length > 1 && path.endsWith("/")) {
     path = path.slice(0, -1);
   }
 
-  var canonicalUrl = origin + path;
+  var canonicalUrl = preferredOrigin + path;
 
-  // Remove ALL existing canonicals (not just one)
   document.querySelectorAll('link[rel="canonical"]').forEach(function(el) {
     el.remove();
   });
 
-  // Add canonical
   var link = document.createElement("link");
   link.setAttribute("rel", "canonical");
   link.setAttribute("href", canonicalUrl);
